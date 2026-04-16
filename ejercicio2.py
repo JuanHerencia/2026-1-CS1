05f30ce1f97d5770e7456410904e12f657e9c34f
JuanHerencia <juanluis.herencia@gmail.com>
2026-04-16 18:41:45 -0500

    Clase3-Ejercicio 2 solucion

--- ejercicio2.py
+++ ejercicio2.py
@@ -9,6 +9,8 @@ Síntomas de alta complejidad:
   Dificultad para entender qué hace la función 
   Alta probabilidad de regresiones al modificar 
   Pruebas unitarias extremadamente complejas 
+-------------------------------------------------
+Solución: Refactorizar para reducir la complejidad
 '''
 # Complejidad ciclomática: 8+ (mala) 
 def procesar_pago(metodo, monto, usuario, pais, es_recurrente): 
@@ -31,3 +33,21 @@ def procesar_pago(metodo, monto, usuario, pais, es_recurrente):
         # más condicionales... 
         pass 
     return resultado 
+
+# Solución
+def obtener_estrategis_pago(metodo, pais, es_recurrente):
+    # Tabla de decisiones en lugar de condicionales
+    estrategia = {
+        ("tarjeta", "PE", True): estrategisSubscripcionVISA(),
+        ("tarjeta", "PE", False): estrategisPagoVISA(),
+        ("tarjeta", "MX", True): estrategisSubscripcionMastercard(),
+        ("Paypal", "*", True): estrategisPayPal(),
+    }
+    key = (metodo, pais, es_recurrente)
+    return estrategia.get(key, EstrategiaDefault()) # EstrategiaDefault() es cuando no encuentra la clave
+
+
+def procesar_pago(metodo, monto, usuario, pais, es_recurrente): 
+    estrategia = obtener_estrategis_pago(metodo, pais, es_recurrente)
+    return estrategia_procesar(monto, usuario)
+
